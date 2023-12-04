bc293021e5a7f97f9a63e821f6f4a98277d6ea1f
Mikad02 <caligiacomo0@gmail.com>
2023-12-04 12:25:37 +0100

    all my fellas tu tu tu

--- test/Main.java
+++ test/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("ciao");
         System.out.println("da dev");
+        System.out.println("all my fellas - mikado");
     }
 }
